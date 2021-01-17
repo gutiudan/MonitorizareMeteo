b91c2c0c341d7d067c47d96d3ccbb9a469a56bd3
gutiu <gutiudan@yahoo.com>
2021-01-17 20:24:27 +0200

    Comentarii clasa City,Country
    Am adaugat comentarii claselor City si
    Country
    
    Signed-off-by: gutiu <gutiudan@yahoo.com>

--- src/main/java/ro/mta/se/lab/Class/City.java
+++ src/main/java/ro/mta/se/lab/Class/City.java
@@ -1,16 +1,33 @@
 package ro.mta.se.lab.Class;
 
+/**
+ * Classname City
+ * Implementeaza clasa City care contine
+ * detele din fisierul de configurare
+ *
+ * @author Dan-Cristian Gutiu
+ */
+
 public class City {
+    /**
+     * Declararea membrilor
+     */
     private int cityId;
     private String cityName;
     private double cityLatitude;
     private double cityLongitude;
     private String countryCode;
 
+    /**
+     * Constructor de clasa City
+     */
     public City(){
 
     }
 
+    /**
+     * Constructor de clasa City cu parametrii
+     */
     public City(int cityId, String cityName, double cityLatitude, double cityLongitude, String countryCode) {
         this.cityId = cityId;
         this.cityName = cityName;
@@ -19,23 +36,23 @@ public class City {
         this.countryCode = countryCode;
     }
 
+    /**
+     * Getere clasa City
+     */
     public String getCityName(){
         return this.cityName;
     }
-
+    public String getCountryCode(){
+        return this.countryCode;
+    }
     public int getCityId(){
         return this.cityId;
     }
-
     public double getCityLatitude(){
         return this.cityLatitude;
     }
-
     public double getCityLongitude(){
         return this.cityLongitude;
     }
 
-    public String getCountryCode(){
-        return this.countryCode;
-    }
 }
--- src/main/java/ro/mta/se/lab/Class/Country.java
+++ src/main/java/ro/mta/se/lab/Class/Country.java
@@ -3,30 +3,53 @@ package ro.mta.se.lab.Class;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+/**
+ * Classname Countru
+ * Implementeaza clasa Country care contine
+ * o lista de orase
+ *
+ * @author Dan-Cristian Gutiu
+ */
+
 public class Country  {
+    /**
+     * Declararea membrilor
+     */
     private ObservableList<City> countryCityList;
     private String countryCode;
 
+    /**
+     * Constructor de clasa Country
+     */
     public Country(){
         this.countryCityList = FXCollections.observableArrayList();
     }
 
+    /**
+     * Constructor de clasa Country
+     * cu parametrii
+     */
     public Country(String countryCode){
         this.countryCode = countryCode;
         this.countryCityList = FXCollections.observableArrayList();
     }
+
+    /**
+     * Functii de adaugare orase
+     */
     public Country(ObservableList<City> countryCityList) {
         this.countryCityList = countryCityList;
     }
-
-    public String getCountryCode(){
-        return countryCode;
-    }
-
     public void Add_City(City city){
         this.countryCityList.add(city);
     }
 
+    /**
+     * Getere clasa City
+     */
+    public String getCountryCode(){
+        return countryCode;
+    }
     public ObservableList<City> getCountryCityList(){
         return countryCityList;
     }
